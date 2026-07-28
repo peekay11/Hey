7c454d8f07d65e06f9c5762dff6256a1cea09761
paseka <pasekamabitsela22@gmail.com>
2026-07-28 18:47:00 +0200

    UI: Update Hey logo typography to use Agbalumo Google Font

--- backend/posts.py
+++ backend/posts.py
@@ -3,4 +3,5 @@ import boto3
  dynamodb = boto3.resource('dynamodb', region_name='af-south-1')
     s3 = boto3.client('s3', region_name='af-south-1')
 
-    
+
+# create a table in dynamo db for the 
