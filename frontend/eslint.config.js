1615edca30981da81f8ce138864d2b44785ade90
paseka <pasekamabitsela22@gmail.com>
2026-07-28 21:00:22 +0200

    addded pipeline file and secrets in gh

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -8,6 +8,7 @@ export default defineConfig([
   globalIgnores(['dist']),
   {
     files: ['**/*.{js,jsx}'],
+    
     extends: [
       js.configs.recommended,
       reactHooks.configs.flat.recommended,
